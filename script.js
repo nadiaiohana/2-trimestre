69dc130fae1bb27b4e1f573c5696b5d5f7a5f2ba
nadiaiohana <130075110+nadiaiohana@users.noreply.github.com>
2024-05-28 10:21:38 -0300

    .

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
+const caixaPrincipal=document.querySelector(".caixa-principal");
+const caixaPerguntas=document.querySelector(".caixa-pergunta");
+const caixaAlternativas=document.querySelector(".caixa-alternativas");
+const caixaResultado=document.querySelector(".caixa-resultado");
+const textoResultado=document.querySelector(".texto-resultado");
 
+const perguntas=[
+  {
+    enunciado:"Qual o menor e o maior país do mundo?",
+    alternativas:[
+      "Mônaco e Canadá",
+      "Vaticano e Rússia",
+    ]
+  }
+  {
+    enunciado:"Quantas casas decimais tem o número pi?",
+    alternativas:[
+      "Milhares",
+      "Infinitas",
+    ]
+  }
+  {
+    enunciado:"Atualmente, quantos elementos químicos a tabela periodica possui?",
+    alternativas:[
+      "122",
+      "118",
+    ]
+  }
+  {
+    enunciado:"O que a palavra legend significa em português?",
+    alternativas:[
+      "Lenda",
+      "Legenda",
+    ]
+  }
+  {
+    enunciado:"Em que período da pré-história o fogo foi descoberto?",
+    alternativas:[
+      "Neolítico",
+      "Paleolítico",
+    ]
+  }
+];
